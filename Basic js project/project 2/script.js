28c00c56ed8157699fbfdb8080a037e42d98b6e4
sunny <sengersunny448@gmail.com>
2023-05-28 23:51:46 +0530

    project 2

---
+++ Basic js project/project 2/script.js
@@ -0,0 +1,64 @@
+
+const sign=document.getElementById("zodiacsign");
+const body=document.getElementsByTagName("body");
+
+console.log(sign.value);
+
+
+const change=() =>{
+switch (sign.value) {
+    case "Aries":
+        body.style.backgroundColor="red";
+
+        
+        break;
+        case "tarus":
+        
+        
+        break;
+        case "gemini":
+        
+        
+        break;
+        case "cancer":
+        
+        
+        break;
+        case "leo":
+        
+        
+        break;
+        case "virgo":
+        
+        
+        break;
+        case "libra":
+        
+        
+        break;
+        case "scorpio":
+        
+        
+        break;
+        case "aquarius":
+        
+        
+        break;
+        case "pisces":
+        
+        
+        break;
+        case "capricon":
+        
+        
+        break;
+        case "saggittarus":
+        
+        
+        break;
+        
+
+    default:
+        break;
+}
+}
